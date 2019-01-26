d8a7e917af21298d4ca97e395e168c93df16c0f8
fhagno@yahoo.com <fhagno@yahoo.com>
2019-01-26 21:04:52 +0100

    Fixing rest calls

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
             .then(resp => resp.json())
             .then((response) => {
                 if (response) {
-                    fetch('https:/peaceful-journey-34211.herokuapp.com/image', {
+                    fetch('/image', {
                         method: 'post',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({id: this.state.user.id})
